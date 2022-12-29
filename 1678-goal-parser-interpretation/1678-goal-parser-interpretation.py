944649767359813ac900c3d8388ba3a14ab7cae0
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2022-12-29 11:04:33 -0500

    Time: 42 ms (62.89%), Space: 13.8 MB (97.00%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -3,8 +3,11 @@ class Solution:
 
 
         command_rep = command.replace("()","o")
+        
+        # to use remove method we should cascade to list
         command_list = list(command_rep)
 
+        # to remove all '(' and ')' we should use iteration
         for _ in command :
             if "(" in command_list or ")" in command_list :
                 command_list.remove('(')
