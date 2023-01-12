206d5b9fbdc56eeeab54fadeb6c60a4c821620aa
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2023-01-12 11:15:13 -0500

    Time: 222 ms (58.29%), Space: 18.1 MB (22.63%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+    
+        dic = {}
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+
+                if i + j not in dic:
+                    dic[i+j] = [mat[i][j]]
+
+                else:
+
+                    dic[i+j].append(mat[i][j])
+
+
+        ans= []
+
+        for entry in dic.items():
+
+            if entry[0] % 2 == 0:
+
+                [ans.append(x) for x in entry[1][::-1]]
+            else:
+                [ans.append(x) for x in entry[1]]
+        return ans
