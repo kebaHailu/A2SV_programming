4e270e681d9e1fb6e42b6efe9f39ee7db7950f1d
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2023-01-31 09:23:41 -0500

    Time: 44 ms (37.66%), Space: 13.8 MB (56.84%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for _ in range(len(nums)):
+            for i in range(len(nums)-1):
+                if nums[i] > nums[i+1]:
+                    nums[i],nums[i+1] = nums[i+1],nums[i]
