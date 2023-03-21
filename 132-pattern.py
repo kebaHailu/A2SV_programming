dcda197db291ecc1665d303d66e0a11f2d60362c
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2023-03-21 16:25:44 +0300

    added 132-pattern.python

---
+++ 132-pattern.py
@@ -0,0 +1,14 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        m_v = nums[0]
+
+        for i in nums[1:]:
+            while stack and i >= stack[-1][0]:
+                stack.pop()
+            if stack and i > stack[-1][1]:
+                return True 
+            stack.append([i , m_v])
+            m_v = min(m_v,i)
+
+        return False
