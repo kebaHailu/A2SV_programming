576a3630c25e4ce821f7838f7581da1480ff2e93
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2023-04-07 10:06:19 +0300

    added number-of-subarrays-with-gcd-equal-to-k.python

--- number-of-subarrays-with-gcd-equal-to-k.py
+++ number-of-subarrays-with-gcd-equal-to-k.py
@@ -1,14 +1,16 @@
 class Solution:
     def subarrayGCD(self, nums: List[int], k: int) -> int:
-        n = len(nums)
         ans = 0
-        for i in range(n):
+
+        for i in range(len(nums)):
             temp = nums[i]
-            for j in range(i,n):
+            for j in range(i,len(nums)):
                 temp = gcd(temp,nums[j])
+
                 if temp == k:
                     ans += 1
-                elif temp < k:
+                
+                if temp < k:
                     break 
         
         return ans
