fc5f311268dd7607825de3d72c7fcf8200c58d90
Kibrom Hailu <92378090+keba-python@users.noreply.github.com>
2023-05-15 09:59:44 +0300

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.py
@@ -0,0 +1,46 @@
+from collections import deque
+from typing import List
+class Solution:
+    #Function to detect cycle in an undirected graph.
+	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+		#Code here
+	
+		visited = set()
+		queue = deque()
+		for vertix in range(V):
+		    if vertix not in visited:
+		        queue.append((vertix , -1))
+		        visited.add(vertix)
+		    while queue:
+		        cur , parent = queue.popleft()
+		        for val in adj[cur]:
+		            if val in visited and val != parent:
+		                return 1
+		            elif val not in visited:
+		                queue.append((val , cur))
+                        visited.add(val)
+                        
+        return 0
+		    
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().split())
+		adj = [[] for i in range(V)]
+		for _ in range(E):
+			u, v = map(int, input().split())
+			adj[u].append(v)
+			adj[v].append(u)
+		obj = Solution()
+		ans = obj.isCycle(V, adj)
+		if(ans):
+			print("1")
+		else:
+			print("0")
+
+# } Driver Code Ends
