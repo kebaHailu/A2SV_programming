4070ffdfd357d97ada45e96bc7e8a028e695010f
Kibrom Hailu <92378090+kebaHailu@users.noreply.github.com>
2023-07-22 17:02:59 +0300

    added minimize-maximum-of-array.python

---
+++ minimize-maximum-of-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        
+        answer = 0
+        prefix_sum = 0 
+
+
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            answer = max(answer, ceil(prefix_sum/(i+1)))
+
+        return answer
