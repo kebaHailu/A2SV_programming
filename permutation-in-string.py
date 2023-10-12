e2feef9dd4417586ac45c527d5ac37e8cadd2e52
Kibrom Hailu <92378090+kebaHailu@users.noreply.github.com>
2023-10-12 16:39:37 +0300

    added permutation-in-string.python

---
+++ permutation-in-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        s1_counter = Counter(s1)
+        s2_counter = Counter()
+        
+        window = len(s1)
+        
+        for idx , ch in enumerate(s2):
+            
+            s2_counter[ch] += 1 
+            
+            if idx >= window:
+                s2_counter[s2[idx-window]] -= 1 
+            
+            if s1_counter == s2_counter:
+                return True 
+            
+        return False
