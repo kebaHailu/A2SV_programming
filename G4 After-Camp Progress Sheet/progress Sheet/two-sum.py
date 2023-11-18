3f8cca0de2408a0bd9ec515d11f767fc671fcb35
Kibrom Hailu <92378090+kebaHailu@users.noreply.github.com>
2023-11-18 14:19:27 +0300

    Added solution for Two Sum

---
+++ G4 After-Camp Progress Sheet/progress Sheet/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in store:
+                return [store[target-nums[i]],i]
+            else:
+                store[nums[i]] = i 
+        
